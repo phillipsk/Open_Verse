ca3eb2a1721503f58147c0d3cfdf9a1d676c500e
phillipsK <kjphillips@suffolk.edu>
2015-08-03 07:56:41 -0400

    python-mysql connection created

--- openV.py
+++ openV.py
@@ -5,16 +5,16 @@
 import MySQLdb
 
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="john", # your username
-                      passwd="megajonhy", # your password
-                      db="jonhydb") # name of the data base
+                     user="openVuser", # your username
+                      passwd="openVpw", # your password
+                      db="openVdev") # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor() 
 
 # Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+cur.execute("SELECT * FROM bible")
 
 # print all the first cell of all the rows
 for row in cur.fetchall() :
